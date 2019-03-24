c8c090caf2cdd3b34d7ecd86c4f0d4cdeb3cdb74
masterUNG <phrombutr@gmail.com>
2019-03-24 10:11:51 +0700

    Start App

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(App());
+// }
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mo Authen',
+      home: Text('Mo Authen'),
+    );
+  }
+}
